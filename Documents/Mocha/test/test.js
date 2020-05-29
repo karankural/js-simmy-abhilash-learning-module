1b0f41d8fb89d4730cca66c9ce81705ee435e888
ghanesh <ghanesh.jain@gmail.com>
2020-05-29 12:25:43 +0530

    Add Two no. program from user input

---
+++ Documents/Mocha/test/test.js
@@ -0,0 +1,20 @@
+function add(a,b){
+a=a+b;
+console.log(a);
+}
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Enter the 1st number to add: ", function(a){
+       rl.question("Enter the 2nd number to add:", function(b){
+          a=parseInt(a);
+         b =  parseInt(b);
+        add(a,b)
+       rl.close();
+ })
+ });
+ rl.on("close", function() {
+     process.exit(0);
+ });
